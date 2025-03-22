bab039d535909dbf8b8bfc9557c9c9b581cdf67b
Jonathan West <jonwest@redhat.com>
2025-03-21 20:16:27 -0400

    Upgrade to Argo Rollouts v1.8.2
    Signed-off-by: Jonathan West <jonwest@redhat.com>

--- hack/run-upstream-argo-rollouts-e2e-tests.sh
+++ hack/run-upstream-argo-rollouts-e2e-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_ROLLOUTS_VERSION=v1.8.0
+CURRENT_ROLLOUTS_VERSION=v1.8.2
 
 function cleanup {
   echo "* Cleaning up"
@@ -127,6 +127,7 @@ set -e
 "$SCRIPT_DIR/verify-rollouts-e2e-tests/verify-e2e-test-results.sh" /tmp/test-e2e.log
 
 echo "* SUCCESS: No unexpected errors occurred."
+
 
 
 
